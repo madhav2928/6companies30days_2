a8d6f53ba6393bb2301733d516cff3ab8ac5899f
madhav2928 <madhav2928@gmail.com>
2023-01-04 21:14:46 +0530

    2nd question completed

---
+++ Microsoft_revision/Minimum_deletions_to_make_array_divisible_2344/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& a) {
+        int res = a[0];
+        for(int i = 1;i<a.size();++i)
+        {
+            res = __gcd(res,a[i]);
+        }
+        int cnt = 0;
+        sort(nums.begin(),nums.end());
+        for(auto i:nums)
+        {
+            if(i==res || res%i==0)
+            {
+                return cnt;
+            }
+            cnt++;
+        }
+        return -1;
+        
+        
+    }
+};
